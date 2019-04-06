27a8c7d91889b12f01e88488becc93b17ff6f109
Bastion Fennell <fennell@csdisco.com>
2019-04-06 15:46:58 -0500

    Style header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import UserPage from './pages/users';
+
+const Header = styled.header`
+    align-items: center;
+    background: #e2e2e2;
+    display: flex;
+    font-size: 24px;
+    height: 75px;
+    justify-content: flex-start;
+    padding: 0 30px;
+    width: 100%;
+`;
+
+const Logo = styled.div`
+    border-radius: 50%;
+    background: #2e3737;
+    height: 45px;
+    margin-right: 20px;
+    width: 45px;
+`;
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <header>
-            Super Hero Co
-        </header>
+        <Header>
+            <Logo />
+            Super Hero Co.
+        </Header>
         <main>
             <UserPage />
         </main>
