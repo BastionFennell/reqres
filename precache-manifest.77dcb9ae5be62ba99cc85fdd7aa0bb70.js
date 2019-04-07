1a246803264df9d86f8567cc7e646505897ffc06
Bastion Fennell <fennell@csdisco.com>
2019-04-07 17:39:39 -0500

    deploy

---
+++ precache-manifest.77dcb9ae5be62ba99cc85fdd7aa0bb70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6cca398c2225b7f42eb",
+    "url": "/reqres/static/js/runtime~main.54625595.js"
+  },
+  {
+    "revision": "ebae9cd52e1e15668c8d",
+    "url": "/reqres/static/js/main.8af4f9a5.chunk.js"
+  },
+  {
+    "revision": "353cad86e29d2e2f40b6",
+    "url": "/reqres/static/js/2.9ce5ec61.chunk.js"
+  },
+  {
+    "revision": "ebae9cd52e1e15668c8d",
+    "url": "/reqres/static/css/main.33dab59a.chunk.css"
+  },
+  {
+    "revision": "7147359df3d0a32f9d70ff1b8b2b6385",
+    "url": "/reqres/index.html"
+  }
+];
